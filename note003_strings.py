3deb6954c471a7cb16e5acd0a374d1ffeabe62e1
Sylvia K <91002274+selgik@users.noreply.github.com>
2022-07-02 22:55:20 +0800

    Create note003_strings.py

---
+++ note003_strings.py
@@ -0,0 +1,11 @@
+#note003_strings.py 
+
+#1. How to write a sentence?
+sen1 = 'Have a wonderful day'
+sen2 = "What a fantastic day"
+sen3 = """
+If you want to write multiple lines like a paragraph
+Add """ in front and in the end
+"""
+
+#2. 
