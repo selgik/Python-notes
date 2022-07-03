c518e59552de1c97f0f9cc2a4763ccfc23c5a8a5
Sylvia K <91002274+selgik@users.noreply.github.com>
2022-07-03 20:18:56 +0800

    Update note003_strings.py

--- note003_strings.py
+++ note003_strings.py
@@ -50,8 +50,48 @@ print("pineT\bapple")                               #\b --> result is pineapple
 print("Fine\tApple")                                #\t --> result is Fine        Apple (like using a tab key)
 
 #6. Using variables
+#1) Version 1: all below can be replaced with %s
+print("I'm %d years old" %20)                       #%d used for integer --> result is I'm 20 years old
+print("I like %s very much" %"coding")              #%s used for strings --> result is I like coding very much
+print("Apple starts with character %c" %"A")        #%c used for character --> result is Apple starts with character A
+print("I like %s and %s" %("football", "tenis"))    #multiple variables can be used as well.
 
+#2) Version 2
+print("I am {} years old" .format(25))              #result is I am 25 years old
+print("I like {} and {}" .format("candy", "milk"))  #result is I like candy and milk
+print("I like {1} and {0}" .format("candy", "milk"))#result is I like milk and candy
 
+#3) Version 3
+print("My name is {name} and I am {age} years old" .format(name="Sylvia", age=20))
+                                                    #result is My name is Sylvia and I am 20 years old
 
+#4) Version 4: from Python ver 3.6+
+age = 20
+name = "Sylvia"
+print(f"My name is {name} and I am {age} years old") #result is My name is Sylvia nad I am 20 years old                                                      
+      
+#7. Exercise
+# Task   : Create a password from the website. Take first 3 digits of web name + length of web name + count of alphabet "e" + $ sign in the end
+# Ex     : https://google.com --> goo + 5 + 1 + $ --> goo61$
+# 1st Try: 
 
+zer = "https://google.com"[7:13]  #extract google
+fir = "https://google.com"[7:10]  #extract goo  
+sec = len(zer)                    #length("google") is 6
+thi = zer.count("e")              #e appears one time in word google
+print(f"{fir}{sec}{thi}$")        #add all together.
 
+# Failure note: It worked for for site:google but if the site name get changed, in ex. facebook, 
+#               I would have to re-count the digit's place and adjust line 78-79, which is manual.
+#               Also, variable names are ambiguous. Right answer should look like below.
+
+# Right answer:
+
+web = "https://google.com"        
+web_c = web.replace("https://", "")  #First, remove the part that is not needed for PW creation.\
+web_c = web_c[:web_c.index(".")]     #Using the same varialbe cut the part after . (.com or .net etc.)
+password = web_c[:3] + str(len(web_c)) + str(web_c.count("e")) + "$"  #combine requirements. 
+print(password)
+
+# Benefit: above codes auto clear https:// part and after . part. I do not have to count digit place either.
+#          variable names are more intuitive and clear. 
