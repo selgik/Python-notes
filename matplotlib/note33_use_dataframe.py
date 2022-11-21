b917880a7c3a39c59395a60d09df08a38c0e67c2
Sylvia K <91002274+selgik@users.noreply.github.com>
2022-11-21 22:51:37 +0800

    Create note33_use_dataframe.py

---
+++ matplotlib/note33_use_dataframe.py
@@ -0,0 +1,21 @@
+#note33_use_dataframe.py
+#prepare libraries and settings
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Batang'
+matplotlib.rcParams['font.size'] = 15
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+
+########## 1. READ EXCEL
+#assume excel file exists in different folder. Tell system to go to parent folder -> go to Pandas folder -> read excel file
+import pandas as pd
+df = pd.read_excel('../Pandas/score.xlsx')    
+
+
+########## 2. SELECT LABELS AND VALUES
+plt.plot(df['app_name'], df['eng'])
+plt.plot(df['app_name'], df['math'])
+        
+   
