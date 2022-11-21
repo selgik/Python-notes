c5ecfaa3bfeddb7ee9f42657a1b5961cfa69b1de
Sylvia K <91002274+selgik@users.noreply.github.com>
2022-11-21 22:47:40 +0800

    Update note32_bar_chart.py

--- matplotlib/note32_bar_chart.py
+++ matplotlib/note32_bar_chart.py
@@ -8,7 +8,7 @@ matplotlib.rcParams['font.size'] = 15
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 
-########## 1. LABEL AND VALUE
+########## 1. BAR CHART
 #label: x-axis
 #value: y-axis
 #of course, you can name the list as x and y:
@@ -16,7 +16,8 @@ matplotlib.rcParams['axes.unicode_minus'] = False
 #1) basic
 labels=['Kang', 'Seo', 'Jeong']
 values=[190,187, 184]
-plt.bar(labels, values)             #result: bar chart where x-axis=labels and y-axis is values
+plt.bar(labels, values)             #result: vertical bar chart where x-axis=labels and y-axis is values
+plt.barh(labels, values)            #result: horizontal bar chart where x-axis=labels and y-axis is values
 
 #2) add color to the bar
 plt.bar(labels, values, color='g')  #result: bar color will be chaanged from blue to green
@@ -28,11 +29,19 @@ plt.bar(labels, values, color=colors, alpha=0.5)    #bar colored in red, green, 
 #4) re-shape so that bar is thinner
 plt.bar(labels, values, width=0.5)
 
+#5) setting hatch (fill bar with patterns)
+#hatch library ref: https://matplotlib.org/stable/gallery/shapes_and_collections/hatch_style_reference.html
+bar=plt.barh(labels, values)
+bar[0].set_hatch('/')   # result: Kang's bar will be filled with /// pattern
+bar[1].set_hatch('x')   # result: Seo's bar will be filled with xxx pattern
+bar[2].set_hatch('..')  # result: Jeong's bar will be filled with ... pattern
+
 
 ########## 2. AXIS: LIMIT, TICKS, LABELS
 #1) limit the range in axis so that difference in data can be easily spotted 
 plt.bar(labels, values, color=colors, alpha=0.5)
-plt.ylim(175, 195)      #instead of y-axis showing 0-200, limit and show from 175 to 195
+plt.ylim(175, 195)      #instead of y-axis showing 0-200, limit and show from 175 to 195. This is for veritcal bar chart 
+plt.xlim(175, 195)      #same as above, for horizontal bar chart
 
 #2) rotate axis label (assume the scenario where data is too many and labels would overlap to each other)
 plt.bar(labels, values, width=0.3)  #obviously, width may need to be adjusted to have all bars visible
@@ -45,6 +54,20 @@ v=[190,187, 184]
 n=['n1', 'n2', 'n3']
 
 plt.bar(l,v)
-plt.xticks(l,n) #instead of showing l as x-axis, show n data. Result: n1, n2, n3 will show in x-axis
+plt.xticks(l,n)                     #instead of showing l as x-axis, show n data. Result: n1, n2, n3 will show in x-axis
 
   
+########## 3. ADD TEXT WITH ENUMERATE()
+#reference on enumerate(): note30_adding_text.py
+#what am I looking for: I want 190, 187, 184 to be printed right above the bar
+
+bar=plt.bar(labels, values)
+plt.ylim(175,195)
+for idx, rect in enumerate(values):
+    plt.text(idx, rect.get_height()+0.5, values[idx], ha='center', color='blue')   #get_height(): get height of rectangular=bar
+    #       x-axis       y-axis       which data to show
+    #plt.text(): need x-axis, y-axis, which data to show?
+
+#result: 190, 187, 184 will show above the bar
+
+     
